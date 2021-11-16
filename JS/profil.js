62a611575148844aac523ca5bd59ceb86247f13c
Anthony LC <gad_77@hotmail.fr>
2021-11-16 19:34:32 +0100

    Projet finalisé

--- JS/profil.js
+++ JS/profil.js
@@ -666,7 +666,6 @@ var photographersData = JSON.parse(JSON.stringify(Photographers));
 var mediaData = JSON.parse(JSON.stringify(media));
 
 //---------------------------------------------------------------------
-//import Photographe from "./class/photographe.js";
 import Photographe from "./class/photographe.js";
 import Media from "./class/medias.js";
 import Tags from "./class/tags.js";
